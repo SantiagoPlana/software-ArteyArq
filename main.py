42d575060097d8d880daf2bc924554d2378939c5
Santiago Plana <santiplana14@gmail.com>
2023-05-15 18:58:17 -0300

    Autocompleción de datos de cada producto seleccionado (parcial)

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ class MainWindow(qtw.QWidget):
         # Layout
         main_layout = qtw.QVBoxLayout()
         grid1 = qtw.QGridLayout()
-        grid2 = qtw.QGridLayout()
+        self.grid2 = qtw.QGridLayout()
         box1 = qtw.QGroupBox(' ')
         self.setLayout(main_layout)
         main_layout.addLayout(grid1)
@@ -127,98 +127,98 @@ class MainWindow(qtw.QWidget):
         grid1.addWidget(qtw.QLabel('Trabajos (por año)'), 2, 3)
         grid1.addWidget(self.trabajos_año, 2, 4)
         # main_layout.addLayout(qtw.QSpacerItem(1, 1), 4, 1, 1, 6)
-        grid2.addWidget(qtw.QLabel('Cliente'), 1, 1)
-        grid2.addWidget(self.cliente, 1, 2, 1, 2)
-        grid2.addWidget(qtw.QLabel('Motivo'), 2, 1)
-        grid2.addWidget(self.motivo, 2, 2, 1, 2)
-        grid2.addWidget(qtw.QLabel('Cant.'), 1, 4)
-        grid2.addWidget(self.cantidad, 2, 4, 1, 1)
+        self.grid2.addWidget(qtw.QLabel('Cliente'), 1, 1)
+        self.grid2.addWidget(self.cliente, 1, 2, 1, 2)
+        self.grid2.addWidget(qtw.QLabel('Motivo'), 2, 1)
+        self.grid2.addWidget(self.motivo, 2, 2, 1, 2)
+        self.grid2.addWidget(qtw.QLabel('Cant.'), 1, 4)
+        self.grid2.addWidget(self.cantidad, 2, 4, 1, 1)
 
         # Fechas
-        grid2.addItem(qtw.QSpacerItem(10, 20), 3, 1)
-        grid2.addWidget(qtw.QLabel('Fecha Recepción'), 4, 1)
-        grid2.addWidget(self.fecha_rec, 4, 2)
-        grid2.addWidget(qtw.QLabel('Fecha Entrega'), 5, 1)
-        grid2.addWidget(self.fecha_entrega, 5, 2)
-        grid2.addWidget(qtw.QLabel('Fecha Realización'), 4, 3)
-        grid2.addWidget(self.fecha_realizacion, 5, 3)
+        self.grid2.addItem(qtw.QSpacerItem(10, 20), 3, 1)
+        self.grid2.addWidget(qtw.QLabel('Fecha Recepción'), 4, 1)
+        self.grid2.addWidget(self.fecha_rec, 4, 2)
+        self.grid2.addWidget(qtw.QLabel('Fecha Entrega'), 5, 1)
+        self.grid2.addWidget(self.fecha_entrega, 5, 2)
+        self.grid2.addWidget(qtw.QLabel('Fecha Realización'), 4, 3)
+        self.grid2.addWidget(self.fecha_realizacion, 5, 3)
 
         # Medidas
-        grid2.addWidget(qtw.QLabel('Med. Orig. cm.'), 1, 6, 2, 1)
-        grid2.addWidget(self.med_orig_cm_alto, 1, 7)
-        grid2.addWidget(self.med_orig_cm_ancho, 2, 7)
+        self.grid2.addWidget(qtw.QLabel('Med. Orig. cm.'), 1, 6, 2, 1)
+        self.grid2.addWidget(self.med_orig_cm_alto, 1, 7)
+        self.grid2.addWidget(self.med_orig_cm_ancho, 2, 7)
 
-        grid2.addWidget(qtw.QLabel('pp. cm'), 1, 8)
-        grid2.addWidget(self.pp_cm, 1, 9)
-        grid2.addWidget(qtw.QLabel('var.'), 2, 8)
-        grid2.addWidget(self.var, 2, 9)
+        self.grid2.addWidget(qtw.QLabel('pp. cm'), 1, 8)
+        self.grid2.addWidget(self.pp_cm, 1, 9)
+        self.grid2.addWidget(qtw.QLabel('var.'), 2, 8)
+        self.grid2.addWidget(self.var, 2, 9)
 
-        grid2.addWidget(qtw.QLabel('Med. Final cm.'), 4, 6, 2, 1)
-        grid2.addWidget(self.med_final_cm_alto, 4, 7)
-        grid2.addWidget(self.med_final_cm_ancho, 5, 7)
+        self.grid2.addWidget(qtw.QLabel('Med. Final cm.'), 4, 6, 2, 1)
+        self.grid2.addWidget(self.med_final_cm_alto, 4, 7)
+        self.grid2.addWidget(self.med_final_cm_ancho, 5, 7)
 
-        grid2.addWidget(qtw.QLabel('Sup. m2:'), 4, 8)
-        grid2.addWidget(self.sup_m2, 4, 9)
-        grid2.addWidget(qtw.QLabel('Per. ml:'), 5, 8)
-        grid2.addWidget(self.per_ml, 5, 9)
+        self.grid2.addWidget(qtw.QLabel('Sup. m2:'), 4, 8)
+        self.grid2.addWidget(self.sup_m2, 4, 9)
+        self.grid2.addWidget(qtw.QLabel('Per. ml:'), 5, 8)
+        self.grid2.addWidget(self.per_ml, 5, 9)
         # Carga de producto
-        grid2.addItem(qtw.QSpacerItem(10, 20), 6, 1)
+        self.grid2.addItem(qtw.QSpacerItem(10, 20), 6, 1)
         # box1.setLayout(grid1)
-        grid2.addWidget(self.label_nombre, 7, 1)
-        grid2.addWidget(self.combo1, 8, 1, 1, 4)
-        grid2.addWidget(self.combo2, 9, 1, 1, 4)
-        grid2.addWidget(self.combo3, 10, 1, 1, 4)
-        grid2.addWidget(self.combo4, 11, 1, 1, 4)
-        grid2.addWidget(self.combo5, 12, 1, 1, 4)
-        grid2.addWidget(self.combo6, 13, 1, 1, 4)
-        grid2.addWidget(self.combo7, 14, 1, 1, 4)
-        grid2.addWidget(self.combo8, 15, 1, 1, 4)
+        self.grid2.addWidget(self.label_nombre, 7, 1)
+        self.grid2.addWidget(self.combo1, 8, 1, 1, 4)
+        self.grid2.addWidget(self.combo2, 9, 1, 1, 4)
+        self.grid2.addWidget(self.combo3, 10, 1, 1, 4)
+        self.grid2.addWidget(self.combo4, 11, 1, 1, 4)
+        self.grid2.addWidget(self.combo5, 12, 1, 1, 4)
+        self.grid2.addWidget(self.combo6, 13, 1, 1, 4)
+        self.grid2.addWidget(self.combo7, 14, 1, 1, 4)
+        self.grid2.addWidget(self.combo8, 15, 1, 1, 4)
         # Stock
-        grid2.addWidget(self.label_stock, 7, 5)
-        grid2.addWidget(self.stock1, 8, 5)
-        grid2.addWidget(self.stock2, 9, 5)
-        grid2.addWidget(self.stock3, 10, 5)
-        grid2.addWidget(self.stock4, 11, 5)
-        grid2.addWidget(self.stock5, 12, 5)
-        grid2.addWidget(self.stock6, 13, 5)
-        grid2.addWidget(self.stock7, 14, 5)
-        grid2.addWidget(self.stock8, 15, 5)
+        self.grid2.addWidget(self.label_stock, 7, 5)
+        self.grid2.addWidget(self.stock1, 8, 5)
+        self.grid2.addWidget(self.stock2, 9, 5)
+        self.grid2.addWidget(self.stock3, 10, 5)
+        self.grid2.addWidget(self.stock4, 11, 5)
+        self.grid2.addWidget(self.stock5, 12, 5)
+        self.grid2.addWidget(self.stock6, 13, 5)
+        self.grid2.addWidget(self.stock7, 14, 5)
+        self.grid2.addWidget(self.stock8, 15, 5)
         # P. Unitario
-        grid2.addWidget(self.label_p_unitario, 7, 6)
-        grid2.addWidget(self.punitario1, 8, 6)
-        grid2.addWidget(self.punitario2, 9, 6)
-        grid2.addWidget(self.punitario3, 10, 6)
-        grid2.addWidget(self.punitario4, 11, 6)
-        grid2.addWidget(self.punitario5, 12, 6)
-        grid2.addWidget(self.punitario6, 13, 6)
-        grid2.addWidget(self.punitario7, 14, 6)
-        grid2.addWidget(self.punitario8, 15, 6)
+        self.grid2.addWidget(self.label_p_unitario, 7, 6)
+        self.grid2.addWidget(self.punitario1, 8, 6)
+        self.grid2.addWidget(self.punitario2, 9, 6)
+        self.grid2.addWidget(self.punitario3, 10, 6)
+        self.grid2.addWidget(self.punitario4, 11, 6)
+        self.grid2.addWidget(self.punitario5, 12, 6)
+        self.grid2.addWidget(self.punitario6, 13, 6)
+        self.grid2.addWidget(self.punitario7, 14, 6)
+        self.grid2.addWidget(self.punitario8, 15, 6)
         # Total
-        grid2.addWidget(self.label_total, 7, 7)
-        grid2.addWidget(self.total1, 8, 7)
-        grid2.addWidget(self.total2, 9, 7)
-        grid2.addWidget(self.total3, 10, 7)
-        grid2.addWidget(self.total4, 11, 7)
-        grid2.addWidget(self.total5, 12, 7)
-        grid2.addWidget(self.total6, 13, 7)
-        grid2.addWidget(self.total7, 14, 7)
-        grid2.addWidget(self.total8, 15, 7)
+        self.grid2.addWidget(self.label_total, 7, 7)
+        self.grid2.addWidget(self.total1, 8, 7)
+        self.grid2.addWidget(self.total2, 9, 7)
+        self.grid2.addWidget(self.total3, 10, 7)
+        self.grid2.addWidget(self.total4, 11, 7)
+        self.grid2.addWidget(self.total5, 12, 7)
+        self.grid2.addWidget(self.total6, 13, 7)
+        self.grid2.addWidget(self.total7, 14, 7)
+        self.grid2.addWidget(self.total8, 15, 7)
         # Otros
-        grid2.addItem(qtw.QSpacerItem(10, 20), 16, 1)
-        grid2.addWidget(self.otro1, 17, 1, 1, 5)
-        grid2.addWidget(self.otro2, 18, 1, 1, 5)
-        grid2.addWidget(self.otro3, 19, 1, 1, 5)
-        grid2.addWidget(self.p_otro1, 17, 7)
-        grid2.addWidget(self.p_otro2, 18, 7)
-        grid2.addWidget(self.p_otro3, 19, 7)
+        self.grid2.addItem(qtw.QSpacerItem(10, 20), 16, 1)
+        self.grid2.addWidget(self.otro1, 17, 1, 1, 5)
+        self.grid2.addWidget(self.otro2, 18, 1, 1, 5)
+        self.grid2.addWidget(self.otro3, 19, 1, 1, 5)
+        self.grid2.addWidget(self.p_otro1, 17, 7)
+        self.grid2.addWidget(self.p_otro2, 18, 7)
+        self.grid2.addWidget(self.p_otro3, 19, 7)
 
-        grid2.addWidget(self.label_p_unitario2, 7, 8, 1, 2)
-        grid2.addWidget(self.punit, 9, 8, 2, 2)
-        grid2.addWidget(self.label_total2, 7, 10, 1, 2)
-        grid2.addWidget(self.total, 9, 10, 2, 2)
-        grid2.addWidget(self.btn_borrar, 11, 9, 2, 2)
+        self.grid2.addWidget(self.label_p_unitario2, 7, 8, 1, 2)
+        self.grid2.addWidget(self.punit, 9, 8, 2, 2)
+        self.grid2.addWidget(self.label_total2, 7, 10, 1, 2)
+        self.grid2.addWidget(self.total, 9, 10, 2, 2)
+        self.grid2.addWidget(self.btn_borrar, 11, 9, 2, 2)
         main_layout.addSpacerItem(qtw.QSpacerItem(10, 30))
-        main_layout.addLayout(grid2)
+        main_layout.addLayout(self.grid2)
 
         #### Combo-boxes ####
         ### Clientes
@@ -246,21 +246,104 @@ class MainWindow(qtw.QWidget):
         self.trabajos_todos.setCompleter(self.completer_trabajos)
         self.clientes_combo.setCompleter(self.completer_clientes)
 
+
+        # Productos
+        # Lamentablemente hay que setear un completer por combobox o si no se borran los valores de uno
+        # cuando se modifica otro...
         self.completer_productos = qtw.QCompleter(
             self.productos.loc[:, 'DenominaciónCompleta'], self)
         self.completer_productos.setCaseSensitivity(qtc.Qt.CaseInsensitive)
         self.completer_productos.setFilterMode(qtc.Qt.MatchContains)
-        # Productos
-        for i in range(grid2.count()):
-            item = grid2.itemAt(i).widget()
-            if isinstance(item, qtw.QComboBox):
-                item.setEditable(True)
-                item.setCompleter(self.completer_productos)
+        self.completer_productos2 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos2.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos2.setFilterMode(qtc.Qt.MatchContains)
+        self.completer_productos3 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos3.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos3.setFilterMode(qtc.Qt.MatchContains)
+        self.completer_productos4 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos4.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos4.setFilterMode(qtc.Qt.MatchContains)
+        self.completer_productos5 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos5.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos5.setFilterMode(qtc.Qt.MatchContains)
+        self.completer_productos6 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos6.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos6.setFilterMode(qtc.Qt.MatchContains)
+        self.completer_productos7 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos7.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos7.setFilterMode(qtc.Qt.MatchContains)
+        self.completer_productos8 = qtw.QCompleter(
+            self.productos.loc[:, 'DenominaciónCompleta'], self)
+        self.completer_productos8.setCaseSensitivity(qtc.Qt.CaseInsensitive)
+        self.completer_productos8.setFilterMode(qtc.Qt.MatchContains)
 
+        self.combo1.activated.connect(lambda: self.complete(string=self.combo1.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo1)))
+        self.combo2.activated.connect(lambda: self.complete(string=self.combo2.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo2)))
+        self.combo3.activated.connect(lambda: self.complete(string=self.combo3.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo3)))
+        self.combo4.activated.connect(lambda: self.complete(string=self.combo4.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo4)))
+        self.combo5.activated.connect(lambda: self.complete(string=self.combo5.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo5)))
+        self.combo6.activated.connect(lambda: self.complete(string=self.combo6.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo6)))
+        self.combo7.activated.connect(lambda: self.complete(string=self.combo7.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo7)))
+        self.combo8.activated.connect(lambda: self.complete(string=self.combo8.currentText(),
+                                                            idx=self.grid2.indexOf(self.combo8)))
+
+        self.combo1.setEditable(True)
+        self.combo2.setEditable(True)
+        self.combo3.setEditable(True)
+        self.combo4.setEditable(True)
+        self.combo5.setEditable(True)
+        self.combo6.setEditable(True)
+        self.combo7.setEditable(True)
+        self.combo8.setEditable(True)
+        self.combo1.setCompleter(self.completer_productos)
+        self.combo2.setCompleter(self.completer_productos2)
+        self.combo3.setCompleter(self.completer_productos3)
+        self.combo4.setCompleter(self.completer_productos4)
+        self.combo5.setCompleter(self.completer_productos5)
+        self.combo6.setCompleter(self.completer_productos6)
+        self.combo7.setCompleter(self.completer_productos7)
+        self.combo8.setCompleter(self.completer_productos8)
+
+
+        #for i in range(self.grid2.count()):
+            # Widget
+        #    item = self.grid2.itemAt(i).widget()
+        #    if isinstance(item, qtw.QComboBox):
+        #        item.setEditable(True)
+        #        item.setCompleter(self.completer_productos)
 
         self.show()
 
     # Methods
+
+    # @qtc.pyqtSlot(str, )
+    def complete(self, string, idx):
+        #print('Hey!')
+        subset = self.productos[
+            self.productos['DenominaciónCompleta'] == string]
+        row, column, cols, rows = self.grid2.getItemPosition(idx)
+        # print(row, column, cols, rows)
+        stock = self.grid2.itemAtPosition(row, 5).widget()
+        p_unit = self.grid2.itemAtPosition(row, 6).widget()
+        total = self.grid2.itemAtPosition(row, 7).widget()
+        try:
+            stock.setText(str(subset.loc[:, 'Stock'].values[0]))
+            p_unit.setText(str(subset.loc[:, 'PrecioUnidad'].values[0]))
+        except Exception as e:
+            print(e)
 
 
 stylesheet = '''
